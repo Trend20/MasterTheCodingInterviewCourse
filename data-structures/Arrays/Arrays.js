25616c180df32a7b67a3eaaec7ef2ac3fc24497f
Trend20 <enockomondi305@gmail.com>
2022-07-12 20:39:33 +0300

    adding array methods

--- data-structures/Arrays/Arrays.js
+++ data-structures/Arrays/Arrays.js
@@ -29,3 +29,69 @@ console.log(items);
 // STATIC ARRAYS(fixed in size)
 
 // DYNAMIC ARRAY(varrying in size)
+
+// CREATING ARRAYS
+
+
+// REVERSE A STRING
+// brutforce
+function reverse(str){
+  let strArr = str.split('').reverse().join('');
+  return strArr;
+}
+
+// solu2
+function reverse(str){
+  let strArray = [];
+  let arrItems = str.length - 1;
+
+  for(let i = arrItems; i>= 0; i--){
+    strArray.push(str[i]);
+  }
+}
+
+// merge sorted array
+let arr1 = [2, 4, 6, 7];
+let arr2 = [ 14, 35, 20, 2, 9];
+
+let combinedArray = arr1.concat(arr2);
+
+for(let i = 0; i<combinedArray.length; i++){
+  if(combinedArray[i] < i){
+    combinedArray.push(i)
+  }
+}
+
+
+
+function mergeSortedArrays(array1, array2){
+  const mergedArray = [];
+  let array1Item = array1[0];
+  let array2Item = array2[0];
+  let i = 1;
+  let j = 1;
+  
+  //We should actually move these 2 if statements to line 2 so that we do the checks before we do assignments in line 3 and 4!
+  if(array1.length === 0) {
+    return array2;
+  }
+  if(array2.length === 0) {
+    return array1;
+  }
+
+  while (array1Item || array2Item){
+   if(array2Item === undefined || array1Item < array2Item){
+     mergedArray.push(array1Item);
+     array1Item = array1[i];
+     i++;
+    }   
+    else {
+      mergedArray.push(array2Item);
+      array2Item = array2[j];
+      j++;
+    }
+   }
+   return mergedArray;
+ }
+ 
+ mergeSortedArrays([0,3,4,31], [3,4,6,30]);
